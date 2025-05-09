a8d139cfa706f8869a12c4f3e17a45d107fb1209
csoler <csoler@odilo.us>
2025-05-09 12:11:37 +0200

    Improvements on subcommands for styles and assets

--- Sources/BrandingCreator/StylesGenerator.swift
+++ Sources/BrandingCreator/StylesGenerator.swift
@@ -10,51 +10,45 @@ import Utils
 
 struct ThemeGenerator: AsyncParsableCommand {
     
-    static let configuration = CommandConfiguration(abstract: "Client styles manager.",
-                                                    subcommands: [ThemeGenerator.Styles.self])
+    static let configuration = CommandConfiguration(abstract: "Theme manager.",
+                                                    subcommands: [ThemeGenerator.Styles.self,
+                                                                  ThemeGenerator.Assets.self])
     
     // MARK: Subcommands
     
     struct Styles: AsyncParsableCommand {
+        static let configuration = CommandConfiguration(abstract: "Generate styles.yml from online theme.")
         
-        struct Generate: AsyncParsableCommand {
-            static let configuration = CommandConfiguration(abstract: "Create new branding.")
+        @Argument(help: "Name of the app to create styles for.")
+        var appName: String
+        
+        @Option(name: [.short, .customLong("xcConfigPath")], help: "Path to xcconfig file. Default is appName.xcconfig on root")
+        var xcConfigPath: String?
+        
+        mutating func run() async throws {
+            let xcConfigPath = self.xcConfigPath ?? "./\(self.appName)/\(self.appName).xcconfig"
+            let xcConfigProperties = XCConfigParser.parse(from: xcConfigPath)
             
-            @Argument(help: "Name of the app to create styles for.")
-            var appName: String
-            
-            @Option(name: [.short, .customLong("xcConfigPath")], help: "Path to xcconfig file. Default is appName.xcconfig on root")
-            var xcConfigPath: String?
-            
-            mutating func run() async throws {
-                let xcConfigPath = self.xcConfigPath ?? "./\(self.appName)/\(self.appName).xcconfig"
-                let xcConfigProperties = XCConfigParser.parse(from: xcConfigPath)
-                
-                try await AssetsManager.generateTheme(self.appName, xcConfigProperties: xcConfigProperties, themeGenerators: [.styles])
-            }
+            try await AssetsManager.generateTheme(self.appName, xcConfigProperties: xcConfigProperties, themeGenerators: [.styles])
         }
         
     }
     
     struct Assets: AsyncParsableCommand {
+        static let configuration = CommandConfiguration(abstract: "Generate assets from online theme.")
         
-        struct Generate: AsyncParsableCommand {
-            static let configuration = CommandConfiguration(abstract: "Create new branding.")
+        @Argument(help: "Name of the app to create assets for.")
+        var appName: String
+        
+        @Option(name: [.short, .customLong("xcConfigPath")], help: "Path to xcconfig file. Default is appName.xcconfig on root")
+        var xcConfigPath: String?
+        
+        mutating func run() async throws {
+            let xcConfigPath = self.xcConfigPath ?? "./\(self.appName)/\(self.appName).xcconfig"
+            let xcConfigProperties = XCConfigParser.parse(from: xcConfigPath)
             
-            @Argument(help: "Name of the app to create styles for.")
-            var appName: String
-            
-            @Option(name: [.short, .customLong("xcConfigPath")], help: "Path to xcconfig file. Default is appName.xcconfig on root")
-            var xcConfigPath: String?
-            
-            mutating func run() async throws {
-                let xcConfigPath = self.xcConfigPath ?? "./\(self.appName)/\(self.appName).xcconfig"
-                let xcConfigProperties = XCConfigParser.parse(from: xcConfigPath)
-                
-                try await AssetsManager.generateTheme(self.appName, xcConfigProperties: xcConfigProperties, themeGenerators: [.assets])
-            }
+            try await AssetsManager.generateTheme(self.appName, xcConfigProperties: xcConfigProperties, themeGenerators: [.assets])
         }
-        
     }
     
 }
