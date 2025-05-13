3dd531a56848da4ef227af7db21a49e61a5f8817
csoler <csoler@odilo.us>
2025-05-13 14:34:21 +0200

    Get assets folder from local path

--- Sources/Utils/Assets/AssetsManager.swift
+++ Sources/Utils/Assets/AssetsManager.swift
@@ -180,7 +180,7 @@ public class AssetsManager {
     }
     
     private static func generateStyles(_ stylesGenerator: AssetsStylesGenerator) async throws {
-        let stylesURL = stylesGenerator.themeURL.appendingPathComponent("variables.xlsx")
+        let stylesURL = URL(filePath: "./BUENA/\(stylesGenerator.appName)/variables.xlsx")
         let (stylesTempURL, _) = try await URLSession.shared.download(from: stylesURL)
         let stylesFileURL = stylesGenerator.brandingFolderURL.appendingPathComponent("variables.xlsx")
         print("STYLES TEMP: \(stylesTempURL)")
